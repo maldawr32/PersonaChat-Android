5492d34e01af0cfcf4b4c6f1b9963589efc84a7f
TajAlden <116644056+maldawr32@users.noreply.github.com>
2026-08-28 12:36:00 +0300

    Add PersonaChat v2 application class

---
+++ app-v2/src/main/java/com/maldawr/personachat/v2/PersonaChatApp.java
@@ -0,0 +1,9 @@
+package com.maldawr.personachat.v2;
+
+import android.app.Application;
+
+public class PersonaChatApp extends Application {
+    @Override public void onCreate() {
+        super.onCreate();
+    }
+}
